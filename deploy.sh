aed35e6be44f635a71b9f3de1e0f6d98b351afbb
Vinicius Reis <vinicius.reis@lionx.com.br>
2022-07-15 19:26:36 -0300

    adjusting specs, att heimdall version

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 fission spec init
 fission env create --spec --name post-custody-env --image nexus.sigame.com.br/python-env-3.8:0.0.5 --builder nexus.sigame.com.br/fission-builder-3.8:0.0.1
 fission fn create --spec --name post-custody-fn --env post-custody-env --src "./func/*" --entrypoint main.post_user_ticket
-fission route create --spec --name post-custody-route --method POST --url /ticket/custody_transfer --function post-custody-fn
+fission route create --spec --name post-custody-route --method POST --url /support/post-ticket-custody-transfer --function post-custody-fn
